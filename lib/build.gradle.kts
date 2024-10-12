8c2e5af64a5a81bdfea537e5e5d5603d9dcf541d
Hossain Khan <hello@hossain.dev>
2024-10-11 21:32:40 -0400

    [ADDED] Modules for lib and sample

---
+++ lib/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm")
+}
+
+group = "dev.hossain.timeline"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(19)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
-rootProject.name = "kgeo-device-timeline"
-
+rootProject.name = "device-timeline"
+include("lib")
+include("sample")
